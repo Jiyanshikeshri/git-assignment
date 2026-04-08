e09f59865a37813b13dfd5d10a45627d701f45bd
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-04-08 15:02:34 +0530

    Added how to calculate Fibonacci series

---
+++ java_training/session1/basic_java/Fibonacci.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    // Method to print Fibonacci series
+    static void printFibonacci(int n) {
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Enter a positive number");
+        } else {
+            printFibonacci(n);
+        }
+        sc.close();
+    }
+}
