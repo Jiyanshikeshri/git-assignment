d30059cdc6ee95d4edeb94b88ae2156d47793743
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-04-15 15:34:35 +0530

    Added UserRepository with dummy users

---
+++ java_training/session3/src/main/java/com/example/session3/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.example.session3.repository;
+
+import com.example.session3.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Repository to store users in memory
+@Repository
+public class UserRepository {
+
+    private List<User> users = new ArrayList<>();
+
+    public UserRepository() {
+        // Adding dummy users initially
+        users.add(new User(1, "Priya", 25, "USER"));
+        users.add(new User(2, "Rahul", 30, "ADMIN"));
+        users.add(new User(3, "Amit", 30, "USER"));
+        users.add(new User(4, "Neha", 28, "USER"));
+        users.add(new User(5, "Rohit", 35, "ADMIN"));
+    }
+
+}
