f3da5007d02399821e31e7444f9b5bfb665312de
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-04-21 14:07:16 +0530

    feat : Created Restaurant entity with ManyToOne Relationship to User

---
+++ Capstone_Restaurant_Orders_Portal/backend/src/main/java/com/example/restaurant_order_portal/entity/Restaurant.java
@@ -0,0 +1,62 @@
+package com.example.restaurant_order_portal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    // Restaurant status (open or closed)
+    @Column(nullable = false)
+    private String status;
+
+    // Many restaurants can belong to one owner, defining ManyToOne relationship
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Restaurant(){
+
+    }
+
+    public Restaurant(String name, String status, User owner) {
+        this.name = name;
+        this.status = status;
+        this.owner = owner;
+    }
+}
