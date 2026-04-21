3ebd9330cb0050f3ff192e361028b2d738a9a237
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-04-21 18:28:26 +0530

    feat : Created Order entity with user and restaurant relationships

---
+++ Capstone_Restaurant_Orders_Portal/backend/src/main/java/com/example/restaurant_order_portal/entity/Order.java
@@ -0,0 +1,91 @@
+package com.example.restaurant_order_portal.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Many orders can belong to a single user therefore, ManyToOne relationship
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Many orders can belong to a single restaurant therefore, ManyToOne relationship
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(nullable = false)
+    private Double totalAmount;
+
+    // Order status (PLACED, PENDING, DELIVERED, COMPLETED, CANCELLED)
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // Auto-set time
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Order() {
+
+    }
+
+    public Order(User user, Restaurant restaurant, Double totalAmount, String status) {
+        this.user = user;
+        this.restaurant = restaurant;
+        this.totalAmount = totalAmount;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
