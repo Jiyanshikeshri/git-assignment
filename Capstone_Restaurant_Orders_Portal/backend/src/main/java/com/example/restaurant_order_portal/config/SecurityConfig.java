171fde1165180aa6cf5502443d12faa39f9680b0
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-04-23 14:18:17 +0530

    feat: configure Spring Security to use JWT filter for API protection

--- Capstone_Restaurant_Orders_Portal/backend/src/main/java/com/example/restaurant_order_portal/config/SecurityConfig.java
+++ Capstone_Restaurant_Orders_Portal/backend/src/main/java/com/example/restaurant_order_portal/config/SecurityConfig.java
@@ -1,20 +1,36 @@
 package com.example.restaurant_order_portal.config;
 
+import com.example.restaurant_order_portal.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable()) // disable csrf for testing
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // allow all requests
-                );
+                        // Public APIs
+                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+
+                        // All other APIs require authentication
+                        .anyRequest().authenticated()
+                )
+
+        // Added JWT filter before Spring's default filter
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
