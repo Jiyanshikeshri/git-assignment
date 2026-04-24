0500f6f4646c04e1099f02226071e9e5308f9e72
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-04-24 08:10:22 +0530

    Implement login and register functionality with API integration and token storage

---
+++ Capstone_Restaurant_Orders_Portal/frontend/js/script.js
@@ -0,0 +1,49 @@
+const BASE_URL = "http://localhost:8080/api/users";
+
+function login() {
+    const data = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+
+    fetch(BASE_URL + "/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        localStorage.setItem("token", data.token);
+
+        alert("Login Successful");
+    });
+}
+
+function register() {
+    const data = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        email: document.getElementById("email").value,
+        phoneNumber: document.getElementById("phoneNumber").value,
+        password: document.getElementById("password").value,
+        role: "USER"
+    };
+
+    fetch(BASE_URL + "/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        alert("Registered Successfully");
+        window.location.href = "index.html";
+    });
+}
