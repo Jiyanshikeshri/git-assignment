5350941d7a0391e10aa67bd1d0c8ac81dcc85cc5
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-06-27 16:18:05 +0530

    feat : add duplicate category validation test

--- Capstone_Assessment_Portal/backend/tests/test_category.py
+++ Capstone_Assessment_Portal/backend/tests/test_category.py
@@ -20,3 +20,43 @@ def test_create_category(client, admin_token):
 
     assert response.status_code == 201
     assert response.json()["message"] == "Category created successfully."
+
+
+
+def test_create_duplicate_category(client, admin_token):
+    """
+    Verifies that duplicate category names are not allowed
+    """
+
+    unique = str(int(time.time()))
+    category_name = f"python_{unique}"
+
+    headers = {
+        "Authorization": f"Bearer {admin_token}"
+    }
+
+    #Creates the Category for the first time
+    response = client.post(
+        "/categories/",
+        headers=headers,
+        json={
+            "name": category_name
+        }
+    )
+
+    assert response.status_code == 201
+
+    # Tries creating the same category again
+    duplicate_response = client.post(
+        "/categories/",
+        headers=headers,
+        json={
+            "name": category_name
+        }
+    )
+
+    assert duplicate_response.status_code == 400
+    assert (
+        duplicate_response.json()["detail"]
+        == "Category with this name already exists."
+    )
