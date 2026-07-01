bc7c722e161073304f5d11e70b38b97c598c10fe
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-07-01 15:58:06 +0530

    feat: add create quiz API test

--- Capstone_Assessment_Portal/backend/tests/test_quiz.py
+++ Capstone_Assessment_Portal/backend/tests/test_quiz.py
@@ -8,3 +8,61 @@ from app.constants.constants import (
     QUIZ_NOT_FOUND,
     CATEGORY_NOT_FOUND,
 )
+
+def test_create_quiz(client, admin_token):
+    """
+    Verifies that an admin can create a new quiz successfully
+    """
+
+    headers = {
+        "Authorization": f"Bearer {admin_token}"
+    }
+
+    unique = uuid.uuid4().hex[:8]
+
+    category_name = f"python_{unique}"
+
+    category_response = client.post(
+        "/categories/",
+        headers=headers,
+        json={
+            "name": category_name
+        }
+    )
+
+    assert category_response.status_code == 201
+
+    categories_response = client.get(
+        "/categories/",
+        headers=headers
+    )
+
+    assert categories_response.status_code == 200
+
+    categories = categories_response.json()
+
+    category_id = None
+
+    for category in categories:
+        if category["name"] == category_name.lower():
+            category_id = category["id"]
+            break
+
+    assert category_id is not None
+
+    quiz_response = client.post(
+        "/quizzes/",
+        headers=headers,
+        json={
+            "title": f"quiz_{unique}",
+            "description": "Python fundamentals quiz",
+            "category_id": category_id,
+            "duration": 30
+        }
+    )
+
+    assert quiz_response.status_code == 201
+    assert (
+        quiz_response.json()["message"]
+        == QUIZ_CREATED_SUCCESSFULLY
+    )
