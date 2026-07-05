d1207dccb01306143f149579461f3c3938459d3a
Jiyanshi Keshri <keshrijiyanshi@gmail.com>
2026-07-06 01:55:49 +0530

    feat: add save partial answer endpoint

--- Capstone_Assessment_Portal/backend/app/routers/attempt_router.py
+++ Capstone_Assessment_Portal/backend/app/routers/attempt_router.py
@@ -2,15 +2,22 @@ from fastapi import (
     APIRouter,
     Depends,
     status,
+    Path,
 )
 
 from app.schemas.attempt_schema import (
     StartAttemptRequest,
     StartAttemptResponse,
+    SaveAnswerRequest,
+)
+
+from app.schemas.common_schema import (
+    MessageResponse,
 )
 
 from app.services.attempt_service import (
     start_quiz_attempt,
+    save_partial_answer,
 )
 
 from app.middleware.auth_middleware import (
@@ -39,4 +46,28 @@ def start_attempt(
     return start_quiz_attempt(
         attempt,
         current_user["user_id"],
+    )
+
+
+@router.patch(
+    "/{attempt_id}/answer",
+    response_model=MessageResponse,
+    status_code=status.HTTP_200_OK,
+)
+def save_answer(
+    attempt_id: str = Path(
+        ...,
+        description="Quiz attempt ID",
+    ),
+    answer: SaveAnswerRequest = ...,
+    current_user=Depends(require_student),
+):
+    """
+    Save or update a student's answer during an ongoing quiz attempt
+    """
+
+    return save_partial_answer(
+        attempt_id=attempt_id,
+        answer=answer,
+        student_id=current_user["user_id"],
     )
